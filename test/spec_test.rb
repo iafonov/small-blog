74fda1b70698e63d4aaecf056827d2eaf5e858f9
Igor Afonov <afonov@gmail.com>
2009-02-03 22:06:59 +0200

    Switched to sinatra edge version.

--- test/spec_test.rb
+++ test/spec_test.rb
@@ -1,16 +1,4 @@
-root_dir = File.expand_path("#{File.dirname(__FILE__)}/..").sub(/^#{Dir.getwd}/, '.')
-
-puts root_dir
-
-$:.unshift "#{root_dir}/sinatra/lib" if File.exist?("#{root_dir}/sinatra")
-$:.unshift "#{root_dir}/lib"
-$:.unshift "#{root_dir}/test"
-
-require 'rubygems'
-require 'spec/interop/test'
-require 'sinatra/test/unit'
-
-load 'main.rb'
+require 'helper.rb'
 
 describe 'my application' do
   it 'should show redirect to about page if accessed from root context' do
